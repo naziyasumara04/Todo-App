6c153a67fa20c4a935925d4b934118123920fba0
Naziya Sumara <naziyasumara1@gmail.com>
2024-03-09 01:44:01 -0800

    Initial Files

---
+++ app.js
@@ -0,0 +1,28 @@
+let input=document.querySelector("input");
+let ul=document.querySelector("ul");
+let btn=document.querySelector("button");
+
+btn.addEventListener("click",function(){
+    let item=document.createElement("li");
+    item.innerText=input.value;
+
+    let delbtn=document.createElement("button");
+    delbtn.innerText="delete";
+    delbtn.classList.add("delete");
+    ul.appendChild(item);
+    item.appendChild(delbtn);
+    input.value="";
+});
+
+ul.addEventListener("click",function(event){
+    if(event.target.nodeName=="BUTTON"){
+     let itemlist=event.target.parentElement;
+     itemlist.remove();
+    }
+});
+
+
+
+
+
+
